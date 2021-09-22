e6a4d74f38c13dbc26bf83e3908cfb6470383df6
Olivier Genez <olivier.genez@gmail.com>
2021-09-22 17:14:50 +1000

    Initial commit

---
+++ app/src/main/java/com/example/testsplashscreen/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.testsplashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
